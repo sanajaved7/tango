f49fcd613e7f5b900b46d9115da80da9ac1bea01
Sana Javed <sanajaved7@gmail.com>
2015-09-21 17:09:02 -0400

    Added links to both views

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hello!")
+	return HttpResponse("Rango says hello! <br/> <br/><a href='/rango/about'>About</a>")
 
 def about(request):
-	return HttpResponse("Rango says here is the about page.")
+	return HttpResponse("Rango says here is the about page. <br/> <br/><a href='/rango/'>Index</a>")
