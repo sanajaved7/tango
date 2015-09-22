e30bb4d956e2e3023a4fb70d09171b257032720a
Sana Javed <sanajaved7@gmail.com>
2015-09-22 12:10:15 -0400

    Added templates

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Rango says hello! <br/> <br/><a href='/rango/about'>About</a>")
+	context_dict = {'boldmessage': "I am bold font from the context"}
+	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
 	return HttpResponse("Rango says here is the about page. <br/> <br/><a href='/rango/'>Index</a>")
