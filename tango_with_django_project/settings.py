d1daf52bc628cc91eceddba60ea298055e87a3ff
Sana Javed <sanajaved7@gmail.com>
2015-10-07 10:27:33 -0400

    Updated project settings

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -13,6 +13,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
