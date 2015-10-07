6efc3dc88e7a5839a32f92398aff10264ae480bb
Sana Javed <sanajaved7@gmail.com>
2015-10-07 14:42:22 -0400

    Updated Category model to use slugify for clean URLs

--- rango/models.py
+++ rango/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Category(models.Model):
 	name = models.CharField(max_length=128, unique=True)
 	views = models.IntegerField(default=0)
 	likes = models.IntegerField(default=0)
+	slug = models.SlugField(unique=True)
+
+	def save(self, *args, **kwargs):
+		self.slug = slugify(self.name)
+		super(Category, self).save(*args, **kwargs)
 
 	def __str__(self):
 		return self.name
@@ -15,4 +21,4 @@ class Page(models.Model):
 	views = models.IntegerField(default=0)
 
 	def __str__(self):
-		return self.title
+		return self.title + ": " + self.url 
