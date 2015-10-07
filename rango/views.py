9525e373d2cac0caea3612c8050133f9c12885c7
Sana Javed <sanajaved7@gmail.com>
2015-10-07 15:34:55 -0400

    Created view for category page

--- rango/views.py
+++ rango/views.py
@@ -2,10 +2,30 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from rango.models import Category, Page
+
 def index(request):
-	context_dict = {'boldmessage': "This is my home page!"}
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'categories': category_list}
 	return render(request, 'rango/index.html', context_dict)
 
 def about(request):
 	context_dict = {'boldmessage': "This is my about page!"}
 	return render(request, 'rango/about.html', context_dict)
+
+def category(request, category_name_slug):
+	context_dict = {}
+
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		context_dict['category_name'] = category.name 
+
+		pages = Page.objects.filter(category=category)
+
+		context_dict['pages'] = pages
+
+		context_dict['category'] = category
+	except Category.DoesNotExist:
+		pass
+
+	return render(request, 'rango/category.html', context_dict)
