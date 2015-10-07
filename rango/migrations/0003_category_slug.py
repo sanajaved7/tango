0a3c23c17c780146ef4d9936b8b880dba5ba5b31
Sana Javed <sanajaved7@gmail.com>
2015-10-07 14:43:46 -0400

    Database migration to reflect slugify in Category model

---
+++ rango/migrations/0003_category_slug.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rango', '0002_auto_20151007_1624'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='category',
+            name='slug',
+            field=models.SlugField(default=datetime.date(2015, 10, 7), unique=True),
+            preserve_default=False,
+        ),
+    ]
