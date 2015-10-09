3035dac2ff1763521fe66dcaf86ebc00510ca617
Sana Javed <sanajaved7@gmail.com>
2015-10-09 16:36:03 -0400

    Created view function to add page

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
-from rango.forms import CategoryForm
+from rango.forms import CategoryForm, PageForm
 
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
@@ -14,7 +14,6 @@ def about(request):
 
 def category(request, category_name_slug):
 	context_dict = {}
-
 	try:
 		category = Category.objects.get(slug=category_name_slug)
 		context_dict['category_name'] = category.name 
@@ -40,3 +39,25 @@ def add_category(request):
 	else:
 		form = CategoryForm()
 	return render(request, 'rango/add_category.html', {'form':form})
+
+def add_page(request, category_name_slug):
+	try:
+		cat = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		cat = None
+
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if cat:
+				page = form.save(commit=False)
+				page.category = cat
+				page.views = 0
+				page.save()
+				return category(request, category_name_slug)
+		else:
+			print form.errors
+	else:
+		form = PageForm()
+	context_dict = {'form':form, 'category':cat}
+	return render(request, 'rango/add_page.html', context_dict)
