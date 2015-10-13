2b8cf4a6b496429a082ed867a920413ea7f32a8a
Sana Javed <sanajaved7@gmail.com>
2015-10-13 11:47:23 -0400

    Registered UserProfile with admin

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
@@ -7,3 +7,4 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page)
+admin.site.register(UserProfile)
