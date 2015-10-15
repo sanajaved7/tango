02830f8813d3747d4fef4d87088cdf03d561b493
Sana Javed <sanajaved7@gmail.com>
2015-10-15 11:10:46 -0400

    Created restricted() view using @login_required decorator

--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
@@ -136,4 +138,6 @@ def user_login(request):
 	else:
 		return render(request, 'rango/login.html', {})
 
-
+@login_required
+def restricted(request):
+	return HttpResponse("Since you're logged in, you can see this text!")
