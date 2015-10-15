0c2e89faa288a0da673cbb71b16e0bfcb37d776f
Sana Javed <sanajaved7@gmail.com>
2015-10-15 10:04:07 -0400

    Added URL mapping for register()

--- rango/urls.py
+++ rango/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
 	url(r'^add_page/$', views.add_page, name='add_page'),
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
+	url(r'^register/$', views.register, name='register'),
 )
