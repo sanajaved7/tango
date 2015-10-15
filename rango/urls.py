f99a9e263bfd9a12051ef648338d8a1f44455ece
Sana Javed <sanajaved7@gmail.com>
2015-10-15 11:54:19 -0400

    Mapped logout view to logout URL

--- rango/urls.py
+++ rango/urls.py
@@ -11,4 +11,5 @@ urlpatterns = patterns('',
 	url(r'^register/$', views.register, name='register'),
 	url(r'^login/$', views.user_login, name='login'),
 	url(r'^restricted/', views.restricted, name='restricted'),
+	url(r'^logout/$', views.user_logout, name='logout'),
 )
