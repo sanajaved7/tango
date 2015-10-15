8cc93d82e8f810d92788e815543bc29e0b40a42d
Sana Javed <sanajaved7@gmail.com>
2015-10-15 10:56:04 -0400

    Created URL for user login page

--- rango/urls.py
+++ rango/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
 	url(r'^register/$', views.register, name='register'),
+	url(r'^login/$', views.user_login, name='login'),
 )
