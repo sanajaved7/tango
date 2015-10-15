b190ae6cc8dbcec8b446039706a034209c9067c2
Sana Javed <sanajaved7@gmail.com>
2015-10-15 11:10:17 -0400

    Created url mapping for the restricted() view

--- rango/urls.py
+++ rango/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
 	url(r'^register/$', views.register, name='register'),
 	url(r'^login/$', views.user_login, name='login'),
+	url(r'^restricted/', views.restricted, name='restricted'),
 )
