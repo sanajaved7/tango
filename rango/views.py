2c992ce72acc73e009fa625294b50d49b019dc46
Sana Javed <sanajaved7@gmail.com>
2015-10-20 11:31:42 -0400

    Updated the restricted page view with new template

--- rango/views.py
+++ rango/views.py
@@ -142,7 +142,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-	return HttpResponse("Since you're logged in, you can see this text!")
+	return render(request, 'rango/restricted.html', {})
 
 #Decorator checks that only logged in users can log out
 @login_required
